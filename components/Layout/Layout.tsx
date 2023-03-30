74a519700cf45c9d996e99494f16e0b88320a001
Chase <chase.seltz21@gmail.com>
2023-03-30 12:31:25 -0700

    Place navbar in Layout, not in home page

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "@/components/Navbar/Navbar";
+import { useTheme } from "@/hooks/useTheme";
+
+interface Props {
+  children?: React.ReactNode;
+}
+
+const Layout = (props: Props) => {
+  const [theme, setTheme] = useTheme();
+
+  return (
+    <div
+      className={`${theme.isDarkTheme && "dark"}
+     ${theme.fontFamily === "serif" && "font-serif"} 
+     ${theme.fontFamily === "sans" && "font-sans"} 
+     ${theme.fontFamily === "mono" && "font-mono"} 
+     container mx-auto text-neutral-800`}
+    >
+      <Navbar theme={theme} setTheme={setTheme} />
+      <main>{props.children}</main>
+    </div>
+  );
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,6 @@
-import Navbar from "@/components/Navbar/Navbar";
-import SearchBar from "@/components/SearchBar/SearchBar";
-import { useTheme } from "@/hooks/useTheme";
 import Head from "next/head";
 
 export default function Home() {
-  const [theme, setTheme] = useTheme();
-
   return (
     <>
       <Head>
@@ -14,16 +9,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main
-        className={`${theme.isDarkTheme && "dark"}
-         ${theme.fontFamily === "serif" && "font-serif"} 
-         ${theme.fontFamily === "sans" && "font-sans"} 
-         ${theme.fontFamily === "mono" && "font-mono"} 
-         container mx-auto text-neutral-800`}
-      >
-        <Navbar theme={theme} setTheme={setTheme} />
-        <SearchBar defaultWord="test word" onSubmit={(e) => console.log(e)} />
-      </main>
     </>
   );
 }
